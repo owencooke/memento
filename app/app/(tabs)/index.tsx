87dc332a1ac5fe394e2eabf0226387a62f734c0c
Owen Cooke <ocooke@ualberta.ca>
2025-02-13 13:09:46 -0700

    markers

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,9 +1,39 @@
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 import { Button, ButtonText } from "@/components/ui/button";
 
 export default function App() {
+  const markers = [
+    {
+      id: 1,
+      title: "Calgary",
+      description: "This is Calgary",
+      coordinate: {
+        latitude: 51.0447,
+        longitude: -114.0719,
+      },
+    },
+    {
+      id: 2,
+      title: "Edmonton",
+      description: "This is Edmonton",
+      coordinate: {
+        latitude: 53.5461,
+        longitude: -113.4938,
+      },
+    },
+    {
+      id: 3,
+      title: "Banff",
+      description: "This is Banff",
+      coordinate: {
+        latitude: 51.1784,
+        longitude: -115.5708,
+      },
+    },
+  ];
+
   return (
     <View className="flex gap-8">
       <Button className="mt-32">
@@ -15,7 +45,22 @@ export default function App() {
             width: "100%",
             height: "100%",
           }}
-        />
+          initialRegion={{
+            latitude: 52.2681,
+            longitude: -113.8112,
+            latitudeDelta: 5,
+            longitudeDelta: 5,
+          }}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.id}
+              coordinate={marker.coordinate}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
       </View>
     </View>
   );
