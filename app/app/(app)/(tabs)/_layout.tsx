c56de5554f49301f5ee6afc18a4c8dd34e7a67c8
Owen Cooke <ocooke@ualberta.ca>
2025-02-15 22:45:26 -0700

    add avatar to header

--- app/app/(app)/(tabs)/_layout.tsx
+++ app/app/(app)/(tabs)/_layout.tsx
@@ -7,15 +7,23 @@ import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallbackText,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import { useSession } from "@/context/AuthContext";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { session } = useSession();
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+        headerShown: true,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
@@ -30,6 +38,18 @@ export default function TabLayout() {
         name="mementos"
         options={{
           title: "Mementos",
+          headerRight: () => (
+            <Avatar size={"md"}>
+              <AvatarFallbackText>
+                {session?.user.user_metadata.full_name}
+              </AvatarFallbackText>
+              <AvatarImage
+                source={{
+                  uri: session?.user.user_metadata.avatar_url,
+                }}
+              />
+            </Avatar>
+          ),
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
