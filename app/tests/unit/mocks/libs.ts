73f649f87e44ae348403fa229919ab2905f4a0ff
Owen Cooke <ocooke@ualberta.ca>
2025-02-22 17:26:01 -0700

    fix jest test

--- app/tests/unit/mocks/libs.ts
+++ app/tests/unit/mocks/libs.ts
@@ -1,3 +1,8 @@
 jest.mock("@react-native-async-storage/async-storage", () =>
   require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
 );
+
+jest.mock("expo-auth-session", () => ({
+  ...jest.requireActual("expo-auth-session"),
+  makeRedirectUri: jest.fn(() => "http://localhost:8081/auth/redirect"),
+}));
