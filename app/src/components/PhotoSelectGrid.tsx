930635348bd50269d1eea93faad9821a3c8bb43f
Owen Cooke <ocooke@ualberta.ca>
2025-02-25 21:16:04 -0700

    add actionsheet

--- app/src/components/PhotoSelectGrid.tsx
+++ app/src/components/PhotoSelectGrid.tsx
@@ -2,10 +2,32 @@ import { View, Text } from "react-native";
 import usePhotos, { Photo } from "../hooks/usePhotos";
 import { Image } from "./ui/image";
 import { Button, ButtonIcon } from "./ui/button";
-import { AddIcon, CloseIcon } from "./ui/icon";
+import {
+  AddIcon,
+  ClockIcon,
+  CloseIcon,
+  DownloadIcon,
+  EditIcon,
+  EyeOffIcon,
+  TrashIcon,
+} from "./ui/icon";
+import {
+  Actionsheet,
+  ActionsheetBackdrop,
+  ActionsheetContent,
+  ActionsheetDragIndicatorWrapper,
+  ActionsheetDragIndicator,
+  ActionsheetItem,
+  ActionsheetIcon,
+  ActionsheetItemText,
+} from "./ui/actionsheet";
+import { useState } from "react";
 
 export default function PhotoSelectGrid() {
+  const [showActionsheet, setShowActionsheet] = useState(false);
   const { hasPermission, addPhotos, photos, removePhoto } = usePhotos();
+
+  const handleClose = () => setShowActionsheet(false);
 
   if (!hasPermission) {
     return <Text>No access to camera</Text>;
@@ -24,12 +46,39 @@ export default function PhotoSelectGrid() {
         size="lg"
         className="w-24 h-24 m-1"
         action="secondary"
-        variant="solid"
-        // OPEN ACTIONSHEET
-        // onPress={() => }
+        onPress={() => setShowActionsheet(true)}
       >
         <ButtonIcon as={AddIcon} />
       </Button>
+      <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
+        <ActionsheetBackdrop />
+        <ActionsheetContent>
+          <ActionsheetDragIndicatorWrapper>
+            <ActionsheetDragIndicator />
+          </ActionsheetDragIndicatorWrapper>
+          <ActionsheetItem
+            onPress={() => {
+              addPhotos("camera");
+              handleClose();
+            }}
+          >
+            <ActionsheetIcon className="stroke-background-700" as={EditIcon} />
+            <ActionsheetItemText>Take a Photo</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem
+            onPress={() => {
+              addPhotos("picker");
+              handleClose();
+            }}
+          >
+            <ActionsheetIcon
+              className="stroke-background-700"
+              as={EyeOffIcon}
+            />
+            <ActionsheetItemText>Select from Library</ActionsheetItemText>
+          </ActionsheetItem>
+        </ActionsheetContent>
+      </Actionsheet>
     </View>
   );
 }
