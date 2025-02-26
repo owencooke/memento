b41602b2da03c3f88128b3622f7932c657ecb13b
Owen Cooke <ocooke@ualberta.ca>
2025-02-26 12:00:00 -0700

    add react hook form

--- app/src/app/(app)/(screens)/(memento)/create.tsx
+++ app/src/app/(app)/(screens)/(memento)/create.tsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Button, ButtonText, ButtonIcon } from "@/src/components/ui/button";
-
 import {
   FormControl,
   FormControlLabel,
   FormControlLabelText,
 } from "@/src/components/ui/form-control";
-import { Input, InputField } from "@/src/components/ui/input";
 import { Heading } from "@/src/components/ui/heading";
 import { ScrollView, View } from "react-native";
 import { Textarea, TextareaInput } from "@/src/components/ui/textarea";
@@ -19,22 +18,43 @@ import { PlayIcon } from "@/src/components/ui/icon";
 import PhotoSelectGrid from "@/src/components/PhotoSelectGrid";
 import { useMutation } from "@tanstack/react-query";
 import { createMementoRouteApiMementoPostMutation } from "@/src/api-client/generated/@tanstack/react-query.gen";
+import { BodyCreateMementoRouteApiMementoPost } from "@/src/api-client/generated/types.gen";
+import { useSession } from "@/src/context/AuthContext";
+import { router } from "expo-router";
 
 export default function CreateMemento() {
   const insets = useSafeAreaInsets();
+  const { session } = useSession();
+  const { control, handleSubmit } =
+    useForm<BodyCreateMementoRouteApiMementoPost>({
+      defaultValues: {
+        memento: {
+          caption: "",
+          date: "",
+          user_id: session?.user.id,
+        },
+        imageMetadata: [],
+        images: [],
+      },
+    });
 
   const createMutation = useMutation(
     createMementoRouteApiMementoPostMutation(),
   );
 
-  const handleAddMemento = () => {};
+  const onSubmit = (body: BodyCreateMementoRouteApiMementoPost) => {
+    createMutation.mutate(
+      { body },
+      {
+        onSuccess: () => router.replace("/(app)/(tabs)/mementos"),
+        onError: (error: any) =>
+          console.error("Failed to create new memento", error),
+      },
+    );
+  };
 
   return (
-    <SafeAreaView
-      // Note: flex-1 is equivalent to h-full for the safe view area
-      className={`flex-1 px-5 pt-5`}
-      edges={["bottom"]}
-    >
+    <SafeAreaView className={`flex-1 px-5 pt-5`} edges={["bottom"]}>
       <ScrollView
         className="flex-1"
         keyboardShouldPersistTaps="handled"
@@ -67,25 +87,52 @@ export default function CreateMemento() {
             <FormControlLabel>
               <FormControlLabelText>Caption</FormControlLabelText>
             </FormControlLabel>
-            <Textarea size="md">
-              <TextareaInput placeholder="ex: an ancient seashell found in Hawaii" />
-            </Textarea>
+            <Controller
+              name="memento.caption"
+              control={control}
+              render={({ field }) => (
+                <Textarea {...field} size="md">
+                  <TextareaInput placeholder="ex: an ancient seashell found in Hawaii" />
+                </Textarea>
+              )}
+            />
           </FormControl>
           <FormControl size={"lg"}>
             <FormControlLabel>
               <FormControlLabelText>Date</FormControlLabelText>
             </FormControlLabel>
-            <DateTimePicker mode="date" value={new Date()} />
+            <Controller
+              name="memento.date"
+              control={control}
+              render={({ field }) => (
+                <DateTimePicker
+                  mode="date"
+                  value={field.value ? new Date(field.value) : new Date()}
+                  onChange={(_, date) => field.onChange(date)}
+                />
+              )}
+            />
           </FormControl>
-          <FormControl size={"lg"}>
+          {/* TODO: figure out how to pass location */}
+          {/* <FormControl size={"lg"}>
             <FormControlLabel>
               <FormControlLabelText>Location</FormControlLabelText>
             </FormControlLabel>
-            <Input>
-              <InputField placeholder="N/A" />
-            </Input>
-          </FormControl>
-          <Button className="mt-auto" size={"lg"}>
+            <Controller
+              name=""
+              control={control}
+              render={({ field }) => (
+                <Input>
+                  <InputField placeholder="N/A" {...field} />
+                </Input>
+              )}
+            />
+          </FormControl> */}
+          <Button
+            className="mt-auto"
+            size={"lg"}
+            onPress={handleSubmit(onSubmit)}
+          >
             <ButtonText>Create Memento</ButtonText>
           </Button>
         </View>
