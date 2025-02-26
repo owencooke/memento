697fe96155ef9dbb1b55f23fcdcbc36a810dd1e7
Owen Cooke <ocooke@ualberta.ca>
2025-02-26 13:42:29 -0700

    fix date from exif

--- app/src/libs/utils/date.ts
+++ app/src/libs/utils/date.ts
@@ -1,3 +1,9 @@
-export const toISODate = (isoString: string): string => {
-  return isoString.split("T")[0];
+export const toISODate = (dateString: string): string => {
+  if (dateString.includes(" ")) {
+    // For exif (YYYY:MM:DD HH:MM:SS)
+    return dateString.split(" ")[0];
+  }
+
+  // For ISO (YYYY-MM-DDTHH:MM:SS)
+  return dateString.split("T")[0];
 };
