e83bf46d286e6a050fc0ff5ed11ae48219337711
Owen Cooke <ocooke@ualberta.ca>
2025-02-26 15:59:40 -0700

    use uuid filename

--- backend/server/services/storage/image.py
+++ backend/server/services/storage/image.py
@@ -1,14 +1,15 @@
+import uuid
 from fastapi import UploadFile
 from server.services.db.config import supabase
 
 
 async def upload_image(file: UploadFile) -> str:
     "Uses Supabase Storage API to upload an image; returns relative path in /images bucket"
+    path = str(uuid.uuid4())
     image_content = await file.read()
     response = supabase.storage.from_("images").upload(
         file=image_content,
-        path=file.filename,
+        path=path,
         file_options={"content-type": file.content_type},
     )
-    print(response)
     return response.path
