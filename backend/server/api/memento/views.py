773bb19e2a2f011784745270ab4be8f228615994
Owen Cooke <ocooke@ualberta.ca>
2025-02-26 00:48:51 -0700

    start creating image upload support

--- backend/server/api/memento/views.py
+++ backend/server/api/memento/views.py
@@ -1,16 +1,36 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, UploadFile
 
-from server.services.db.models.schema_public_latest import Memento, MementoInsert
+from pydantic import BaseModel
+from server.services.db.models.schema_public_latest import (
+    ImageInsert,
+    Memento,
+    MementoInsert,
+)
+from server.services.db.queries.image import upload_image
 from server.services.db.queries.memento import create_memento
 
 router = APIRouter()
 
 
-@router.post("/", response_model=Memento)
-def create_memento_route(memento: MementoInsert) -> Memento:
-    """Creates a new memento."""
-    try:
-        new_memento = create_memento(memento)
-        return new_memento
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+class MementoAndImages(BaseModel):
+    memento: MementoInsert
+    imageMetadata: list[ImageInsert]
+
+
+@router.post("/")
+async def create_memento_route(
+    body: MementoAndImages,
+    files: list[UploadFile] = File(...),
+) -> Memento:
+    """Creates a new memento, uploads associated images to object storage, and stores image metadata."""
+
+    # Create new memento record
+    new_memento = create_memento(body.memento)
+
+    for i in range(len(files)):
+        # Upload image to object storage
+        path = upload_image(files[i])
+
+        # Create new image metadata records
+
+    return new_memento
---
+++ backend/server/services/db/queries/image.py
@@ -0,0 +1,14 @@
+from fastapi import UploadFile
+from server.services.db.config import supabase
+
+
+async def upload_image(file: UploadFile) -> str:
+    "Uses Supabase Storage API to upload an image; returns relative path in /images bucket"
+    image_content = await file.read()
+    response = supabase.storage.from_("avatars").upload(
+        file=image_content,
+        path=file.filename,
+        file_options={"content-type": file.content_type},
+    )
+    print(response)
+    return response.path
