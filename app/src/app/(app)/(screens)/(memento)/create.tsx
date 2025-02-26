ba5f3b2783e6d03848fa87ca979609eb374b44e4
Owen Cooke <ocooke@ualberta.ca>
2025-02-25 20:29:51 -0700

    add custom back header for stack

--- app/src/app/(app)/(screens)/(memento)/create.tsx
+++ app/src/app/(app)/(screens)/(memento)/create.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Button, ButtonText, ButtonIcon } from "@/src/components/ui/button";
+
 import {
   FormControl,
   FormControlLabel,
@@ -8,25 +11,33 @@ import { Heading } from "@/src/components/ui/heading";
 import { View } from "react-native";
 import { Textarea, TextareaInput } from "@/src/components/ui/textarea";
 import DateTimePicker from "@react-native-community/datetimepicker";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import { PlayIcon } from "@/src/components/ui/icon";
 export default function CreateMemento() {
   const handleAddMemento = () => {
     // router.push("/")
   };
   return (
-    <View className={`w-full h-full flex justify-center`}>
-      <Heading size="2xl">Create Memento</Heading>
+    <SafeAreaView className={`w-full h-full flex justify-center gap-4 px-5`}>
+      <View className="flex flex-row justify-between items-center">
+        <Heading className="block" size="2xl">
+          Create Memento
+        </Heading>
+        <Button size="lg" className="p-3.5" action="secondary" variant="solid">
+          <ButtonIcon as={PlayIcon} />
+        </Button>
+      </View>
       <FormControl size={"lg"}>
         <FormControlLabel>
           <FormControlLabelText>Caption</FormControlLabelText>
         </FormControlLabel>
         <Textarea size="md">
-          <TextareaInput placeholder="Your text goes here..." />
+          <TextareaInput placeholder="ex: an ancient seashell found in Hawaii" />
         </Textarea>
       </FormControl>
       <FormControl size={"lg"}>
         <FormControlLabel>
-          <FormControlLabelText>Password</FormControlLabelText>
+          <FormControlLabelText>Date</FormControlLabelText>
         </FormControlLabel>
         <DateTimePicker mode="date" value={new Date()} />
       </FormControl>
@@ -38,6 +49,9 @@ export default function CreateMemento() {
           <InputField placeholder="N/A" />
         </Input>
       </FormControl>
-    </View>
+      <Button className="mt-auto" size={"lg"}>
+        <ButtonText>Create Memento</ButtonText>
+      </Button>
+    </SafeAreaView>
   );
 }
