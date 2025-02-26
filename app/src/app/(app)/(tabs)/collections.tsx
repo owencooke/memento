060b271655c6ff49654551ddbb109c8bfff6ee8c
Levi Thomas <levi.thomas5384@gmail.com>
2025-02-25 17:23:57 -0700

    Collections Tab Fetch & Display Collections

--- app/src/app/(app)/(tabs)/collections.tsx
+++ app/src/app/(app)/(tabs)/collections.tsx
@@ -1,73 +1,61 @@
-import { Button, ButtonIcon, ButtonText } from "@/src/components/ui/button";
 import { Text } from "@/src/components/ui/text";
 import usePhotos from "@/src/hooks/usePhotos";
-import { Image } from "@/src/components/ui/image";
-import { View } from "react-native";
-import { ScrollView } from "react-native";
-import { TrashIcon } from "@/src/components/ui/icon";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "@/src/context/AuthContext";
-import { userInfoApiUserIdGetOptions } from "@/src/api-client/generated/@tanstack/react-query.gen";
+import { getCollectionsApiUserIdCollectionsGetOptions } from "@/src/api-client/generated/@tanstack/react-query.gen";
+import { Box } from "@/src/components/ui/box";
+import { Fab, FabLabel, FabIcon } from "@/src/components/ui/fab";
+import { AddIcon } from "@/src/components/ui/icon";
+import { FlatList } from "react-native";
 
 export default function Collections() {
   const { session } = useSession();
   const { hasPermission, addPhotos, photos, removePhoto } = usePhotos();
   const {
-    data: userInfo,
+    data: collections,
     error,
     isLoading,
   } = useQuery({
-    ...userInfoApiUserIdGetOptions({
+    ...getCollectionsApiUserIdCollectionsGetOptions({
       path: {
         id: session?.user.id ?? "",
       },
     }),
+    enabled: !!session?.user.id,
   });
 
-  console.log({ userInfo, error, isLoading });
+  // console.log({ userInfo, error, isLoading });
+  console.log({ collections, error, isLoading });
 
-  if (!hasPermission) {
-    return <Text>No access to camera</Text>;
-  }
+  if (!hasPermission) return <Text>No access to camera</Text>;
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error fetching collections.</Text>;
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        flexGrow: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingBlock: 8,
-      }}
-    >
-      <Text className="text-center w-full">
-        Your Birthday: {userInfo?.birthday}
-      </Text>
-      <Button onPress={() => addPhotos("camera")}>
-        <ButtonText>Take a photo</ButtonText>
-      </Button>
-      <Button className="my-4" onPress={() => addPhotos("picker")}>
-        <ButtonText>Select from camera roll</ButtonText>
-      </Button>
-      <>
-        {photos.map((photo, index) => (
-          <View key={index}>
-            <Image source={photo.uri} size="xl" alt="" />
-            {photo.exif && (
-              <Text style={{ marginTop: 5 }}>
-                {Object.entries(photo.exif).map(([key, value]) => (
-                  <Text key={key}>{`${key}: ${value}\n`}</Text>
-                ))}
-              </Text>
+    <Box className="h-full w-full bg-background-50 rounded-md">
+      <FlatList
+        numColumns={2}
+        data={collections}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <Box className="p-4 border-b border-gray-200">
+            <Text className="text-lg font-bold">{item.title}</Text>
+            {item.caption && (
+              <Text className="text-gray-500">{item.caption}</Text>
             )}
-            <Button
-              size="lg"
-              className="rounded-full p-3.5"
-              onPress={() => removePhoto(photo)}
-            >
-              <ButtonIcon as={TrashIcon} />
-            </Button>
-          </View>
-        ))}
-      </>
-    </ScrollView>
+          </Box>
+        )}
+      />
+
+      <Fab
+        size="md"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+      >
+        <FabIcon as={AddIcon} />
+      </Fab>
+    </Box>
   );
 }
