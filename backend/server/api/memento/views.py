a9b19113b01906bea3e2bb7322922b61d48ff93a
Owen Cooke <ocooke@ualberta.ca>
2025-02-27 00:22:00 -0700

    add get mementos endpoint

--- backend/server/api/memento/views.py
+++ backend/server/api/memento/views.py
@@ -1,7 +1,9 @@
 import json
 from typing import Annotated
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter, Depends, File, Form, UploadFile
 
+from pydantic import UUID4
+from server.api.path import get_user_id
 from server.services.db.models.schema_public_latest import (
     ImageInsert,
     Memento,
@@ -9,16 +11,23 @@ from server.services.db.models.schema_public_latest import (
 )
 from server.services.db.queries.image import create_image_metadata
 from server.services.storage.image import upload_image
-from server.services.db.queries.memento import create_memento
+from server.services.db.queries.memento import create_memento, get_mementos
 
 router = APIRouter()
 
 
+@router.get("/")
+def get_users_mementos(user_id: UUID4 = Depends(get_user_id)):
+    """Gets all the mementos belonging to a user."""
+    return get_mementos(user_id)
+
+
 @router.post("/")
-async def create_memento_route(
+async def create_new_memento(
     memento: Annotated[str, Form()],
     imageMetadata: Annotated[str, Form()],
     images: Annotated[list[UploadFile], File()],
+    user_id: UUID4 = Depends(get_user_id),
 ) -> Memento:
     """Creates a new memento, uploads associated images to object storage, and stores image metadata."""
 
@@ -29,6 +38,7 @@ async def create_memento_route(
     ]
 
     # Create new memento record
+    memento_data.user_id = user_id
     new_memento = create_memento(memento_data)
 
     for i in range(len(images)):
--- backend/server/api/router.py
+++ backend/server/api/router.py
@@ -2,7 +2,12 @@ from fastapi.routing import APIRouter
 
 from server.api import monitoring, user, memento
 
+# Nested routers that require user id access
+user_id_router = APIRouter(prefix="/user/{id}")
+user_id_router.include_router(memento.router, prefix="/memento", tags=["memento"])
+
+# Main API router
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(user.router, prefix="/user", tags=["user"])
-api_router.include_router(memento.router, prefix="/memento", tags=["memento"])
+api_router.include_router(user_id_router)
