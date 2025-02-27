002b308897c465084b6b7ef67ab0ce16e5a33e49
Owen Cooke <ocooke@ualberta.ca>
2025-02-27 00:39:52 -0700

    fix user_id path param

--- app/src/app/(app)/(tabs)/mementos.tsx
+++ app/src/app/(app)/(tabs)/mementos.tsx
@@ -1,12 +1,27 @@
 import { Center } from "@/src/components/ui/center";
 import { Fab, FabIcon } from "@/src/components/ui/fab";
 import { AddIcon } from "@/src/components/ui/icon";
+import { useSession } from "@/src/context/AuthContext";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useQuery } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { ScrollView } from "react-native";
 
 export default function Mementos() {
   const tabBarHeight = useBottomTabBarHeight();
+  const { session } = useSession();
+
+  //   const {
+  //     data: userInfo,
+  //     error,
+  //     isLoading,
+  //   } = useQuery({
+  //     ...userInfoApiUserIdGetOptions({
+  //       path: {
+  //         id: session?.user.id ?? "",
+  //       },
+  //     }),
+  //   });
 
   const handleAddMemento = () => {
     router.push("/(app)/(screens)/(memento)/create");
