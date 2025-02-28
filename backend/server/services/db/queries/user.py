da9c1db8e092867da366b958d1d98abc731d759f
Levi Thomas <levi.thomas5384@gmail.com>
2025-02-28 11:20:05 -0700

    Move collection queries

--- backend/server/services/db/queries/user.py
+++ backend/server/services/db/queries/user.py
@@ -3,8 +3,6 @@ from pydantic import UUID4
 
 from server.services.db.config import supabase
 from server.services.db.models.schema_public_latest import (
-    Collections,
-    CollectionsInsert,
     UserInfo,
 )
 
@@ -13,32 +11,3 @@ def get_user_info(id: UUID4) -> UserInfo:
     """Gets user info from DB table for specific user id."""
     response = supabase.table("user_info").select("*").eq("id", id).execute()
     return UserInfo(**response.data[0])
-
-
-def fetch_collections(user_id: UUID4) -> list[Collections]:  # Updated function name
-    """Gets collections from DB table for specific user_id."""
-    response = (
-        supabase.table("collections").select("*").eq("user_id", str(user_id)).execute()
-    )
-
-    collections = response.data
-    return (
-        [Collections(**collection) for collection in collections] if collections else []
-    )
-
-
-def insert_collection(collection: CollectionsInsert) -> CollectionsInsert:
-    """Inserts a collection in the database."""
-    response = (
-        supabase.table("collections")
-        .insert(
-            {
-                "user_id": str(collection.user_id),
-                "title": collection.title,
-                "caption": collection.caption,
-            }
-        )
-        .execute()
-    )
-
-    return CollectionsInsert(**response.data[0])
