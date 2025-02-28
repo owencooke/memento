e4e0656593b7e2323347f6ff5cc424e5c5718798
Levi Thomas <levi.thomas5384@gmail.com>
2025-02-28 11:20:45 -0700

    Get Collection includes associated mementos

---
+++ backend/server/services/db/queries/collection.py
@@ -0,0 +1,40 @@
+from pydantic import UUID4
+
+from server.services.db.config import supabase
+from server.services.db.models.joins import CollectionWithMementos
+from server.services.db.models.schema_public_latest import (
+    Collection,
+    CollectionInsert,
+)
+from server.services.db.utils import convert_to_supabase_types
+
+
+def get_collections(
+    user_id: UUID4,
+) -> list[CollectionWithMementos]:
+    """Gets collections and associated mementos for a user."""
+    response = (
+        supabase.table("collection")
+        .select("*, has_memento(*)")
+        .eq("user_id", str(user_id))
+        .execute()
+    )
+    collections = response.data or []
+
+    formatted_collections = []
+    for collection in collections:
+        mementos = list(collection.get("has_memento", []))
+        formatted_collections.append(
+            CollectionWithMementos(**collection, mementos=mementos),
+        )
+    return formatted_collections
+
+
+def create_collection(new_collection: CollectionInsert) -> Collection:
+    """Creates a new collection for a user."""
+    response = (
+        supabase.table("collections")
+        .insert(convert_to_supabase_types(new_collection))
+        .execute()
+    )
+    return Collection(**response.data[0])
