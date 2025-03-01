d2c6ccff863508b7572e3b81c9e34417ae22b211
Levi Thomas <levi.thomas5384@gmail.com>
2025-03-01 15:49:03 -0700

    Collection Card Component

---
+++ app/src/components/cards/CollectionCard.tsx
@@ -0,0 +1,59 @@
+import { View } from "react-native";
+import { Image } from "@/src/components/ui/image";
+import { CollectionWithMementos } from "@/src/api-client/generated";
+import { Text } from "@/src/components/ui/text";
+import { useMemo } from "react";
+import { Pressable } from "@/src/components/ui/pressable";
+import { Box } from "@/src/components/ui/box";
+
+export default function CollectionCard({
+  id,
+  title,
+  date,
+  location,
+  mementos,
+  onPress,
+  selected,
+}: CollectionWithMementos & { onPress: () => void; selected: boolean }) {
+  //
+  // Get thumbnail from first memento
+  const thumbnail = null;
+
+  // Extract city from whole location
+  const city = useMemo(
+    () => (location ? location.split(",")[0].trim() : location),
+    [location],
+  );
+
+  return (
+    <Pressable className="flex-1" onPress={onPress}>
+      <Box
+        className={`flex-1 gap-4 p-3 rounded-xl shadow-hard-3 ${
+          selected ? "bg-gray-300" : "bg-background-0"
+        }`}
+      >
+        <Box className="aspect-square">
+          <Image
+            source={{ uri: "https://placehold.co/400.png" }}
+            className="w-full h-full"
+            alt=""
+            resizeMode="cover"
+          />
+        </Box>
+        <Box className="flex flex-1 justify-between gap-1">
+          <Text size="lg" className="font-bold text-center flex-1">
+            {title}
+          </Text>
+          <View className="flex flex-row justify-between items-center mt-auto font-medium">
+            <Text className="flex-1" size="sm">
+              {date}
+            </Text>
+            <Text className="flex-1" size="sm">
+              {city}
+            </Text>
+          </View>
+        </Box>
+      </Box>
+    </Pressable>
+  );
+}
