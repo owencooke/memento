360201c4a2b5e47af0ecfdbce8bfe01c003de90f
Levi Thomas <levi.thomas5384@gmail.com>
2025-03-02 16:57:33 -0700

    Add CoordinatesInsert to NewCollection

--- backend/server/api/collection/models.py
+++ backend/server/api/collection/models.py
@@ -1,10 +1,10 @@
-from server.services.db.models.gis import BaseWithCoordinates
+from server.services.db.models.gis import BaseWithCoordinates, CoordinatesInsert
 from server.services.db.models.schema_public_latest import CollectionInsert
 
 # Note: the ignores are for MyPy, to ignore "coordinates" field type differences
 
 
-class NewCollection(BaseWithCoordinates, CollectionInsert):  # type: ignore[misc]
+class NewCollection(CoordinatesInsert, BaseWithCoordinates, CollectionInsert):  # type: ignore[misc]
     """Inserting a new Collection to the DB.
 
     Overrides coordinates with proper Pydantic model.
