f37efc648ff1d2185b3d3dbe7934eff473bc9d84
Owen Cooke <ocooke@ualberta.ca>
2025-03-02 02:00:08 -0700

    fix IOS render

--- app/src/components/inputs/DatePickerInput.tsx
+++ app/src/components/inputs/DatePickerInput.tsx
@@ -4,13 +4,18 @@ import { Input, InputField, InputSlot, InputIcon } from "../ui/input";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { toISODateString } from "@/src/libs/date";
 import { Box } from "../ui/box";
-import { Pressable } from "react-native";
+import { Pressable, Platform } from "react-native";
 
 interface DatePickerInputProps {
   value: Date | null;
-  onChange?: (value: Date) => void;
+  onChange?: (value: Date | undefined) => void;
 }
 
+/**
+ * DateTimePicker renders input field for IOS, but not Android.
+ * This component unifies both platforms.
+ * Reference: https://github.com/owencooke/memento/issues/285
+ */
 const DatePickerInput = ({
   value,
   onChange = (_) => {},
@@ -23,31 +28,34 @@ const DatePickerInput = ({
 
   return (
     <>
-      <Box className="relative w-full">
-        <Pressable
-          className="inset-0 absolute z-10"
-          onPress={handleToggleModal}
-        />
-        <Input className="bg-background-0">
-          <InputField
-            value={value ? toISODateString(value) : ""}
-            placeholder="Select a date"
-            editable={false}
-            pointerEvents="none"
+      {Platform.OS === "android" && (
+        <Box className="relative w-full">
+          <Pressable
+            className="inset-0 absolute z-10"
+            onPress={handleToggleModal}
           />
-          <InputSlot>
-            <InputIcon as={CalendarDaysIcon} className="mr-2" />
-          </InputSlot>
-        </Input>
-      </Box>
-      {showDatePicker && (
+          <Input className="bg-background-0">
+            <InputField
+              value={value ? toISODateString(value) : ""}
+              placeholder="Select a date"
+              editable={false}
+              pointerEvents="none"
+            />
+            <InputSlot>
+              <InputIcon as={CalendarDaysIcon} className="mr-2" />
+            </InputSlot>
+          </Input>
+        </Box>
+      )}
+      {(Platform.OS === "ios" || showDatePicker) && (
         <DateTimePicker
           mode="date"
           value={value ?? new Date()}
           onChange={(_, date) => {
             setShowDatePicker(false);
-            if (date) onChange(date);
+            onChange(date);
           }}
+          maximumDate={new Date()}
         />
       )}
     </>
