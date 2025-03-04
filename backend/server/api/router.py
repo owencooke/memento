ca4a442c23dcfc0f1a348f609e9a2081511b677e
Levi Thomas <levi.thomas5384@gmail.com>
2025-03-04 09:24:44 -0700

    lint

--- backend/server/api/router.py
+++ backend/server/api/router.py
@@ -2,7 +2,6 @@ from fastapi.routing import APIRouter
 
 from server.api import collection, memento, monitoring, user
 
-
 # Nested routers that require user id access
 user_id_router = APIRouter(prefix="/user/{user_id}")
 user_id_router.include_router(memento.router, prefix="/memento", tags=["memento"])
