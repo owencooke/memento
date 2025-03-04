e6939537287374d2e9ca1d4d51ce3707951381cf
Owen Cooke <ocooke@ualberta.ca>
2025-03-03 18:02:02 -0700

    switch back to non modular

--- app/src/components/inputs/PhotoSelectGrid.tsx
+++ app/src/components/inputs/PhotoSelectGrid.tsx
@@ -3,7 +3,7 @@ import usePhotos, { Photo } from "../../hooks/usePhotos";
 import { Image } from "../ui/image";
 import { Button, ButtonIcon } from "../ui/button";
 import { AddIcon, CloseIcon } from "../ui/icon";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import DraggableGrid from "react-native-draggable-grid";
 import PhotoSourceSheet from "./PhotoSourceSheet";
 
@@ -11,6 +11,7 @@ interface GridItem {
   key: string;
   photo: Photo | null;
 }
+
 interface PhotoSelectGridProps {
   onChange: (photos: Photo[]) => Promise<void>;
 }
@@ -24,49 +25,23 @@ export default function PhotoSelectGrid({ onChange }: PhotoSelectGridProps) {
     onChange(photos).catch((e) => console.error(e));
   }, [onChange, photos]);
 
-  const gridData = useMemo(
-    () => [
-      ...photos.map((photo, index) => ({
-        key: `photo-${index}`,
-        photo,
-      })),
-      { key: "add-button", photo: null },
-    ],
-    [photos],
-  );
-
-  const renderItem = (item: GridItem) => {
-    const { photo } = item;
-    if (photo === null) {
-      return (
-        // Render Add Button
-        <View className="p-1 flex-1 aspect-square">
-          <Button
-            size="lg"
-            className="w-full h-full"
-            action="secondary"
-            onPress={() => setShowActionsheet(true)}
-          >
-            <ButtonIcon as={AddIcon} />
-          </Button>
-        </View>
-      );
-    }
-    return (
-      <InteractivePhotoCard photo={photo} onDelete={() => removePhoto(photo)} />
-    );
-  };
-
   if (!hasPermission) {
     return <Text>No access to camera</Text>;
   }
+
+  const gridData: GridItem[] = [
+    ...photos.map((photo, index) => ({
+      key: `photo-${index}`,
+      photo,
+    })),
+    { key: "add-button", photo: null },
+  ];
 
   return (
     <View className="flex-1">
       <DraggableGrid
         numColumns={3}
         data={gridData}
-        renderItem={renderItem}
         onDragRelease={(data) => {
           // Filter out the add button and update the photos array
           const newPhotos = data
@@ -74,37 +49,50 @@ export default function PhotoSelectGrid({ onChange }: PhotoSelectGridProps) {
             .map((item: GridItem) => item.photo) as Photo[];
           setPhotos(newPhotos);
         }}
+        renderItem={(item: GridItem) => {
+          // Add Button Card
+          if (item.photo === null) {
+            return (
+              <View className="p-1 flex-1 aspect-square">
+                <Button
+                  size="lg"
+                  className="w-full h-full"
+                  action="secondary"
+                  onPress={() => setShowActionsheet(true)}
+                >
+                  <ButtonIcon as={AddIcon} />
+                </Button>
+              </View>
+            );
+          }
+
+          // Photo Cards
+          return (
+            <View className="p-1 flex-1">
+              <View className="relative overflow-hidden rounded-md aspect-square">
+                <Image
+                  source={{ uri: item.photo.uri }}
+                  className="w-full h-full"
+                  alt=""
+                  resizeMode="cover"
+                />
+                <Button
+                  onPress={() => item.photo && removePhoto(item.photo)}
+                  className="absolute p-1 rounded-full top-1 right-1"
+                  size="sm"
+                >
+                  <ButtonIcon className="m-0 p-0" as={CloseIcon} />
+                </Button>
+              </View>
+            </View>
+          );
+        }}
       />
       <PhotoSourceSheet
         addPhotos={addPhotos}
         visible={showActionsheet}
         setVisible={setShowActionsheet}
       />
-    </View>
-  );
-}
-
-interface InteractivePhotoCardProps {
-  photo: Photo;
-  onDelete: () => void;
-}
-
-function InteractivePhotoCard({ photo, onDelete }: InteractivePhotoCardProps) {
-  return (
-    <View className="p-1 flex-1 relative overflow-hidden rounded-md aspect-square">
-      <Image
-        source={{ uri: photo.uri }}
-        className="w-full h-full"
-        alt=""
-        resizeMode="cover"
-      />
-      <Button
-        onPress={onDelete}
-        className="absolute p-1 rounded-full top-1 right-1"
-        size="sm"
-      >
-        <ButtonIcon className="m-0 p-0" as={CloseIcon} />
-      </Button>
     </View>
   );
 }
