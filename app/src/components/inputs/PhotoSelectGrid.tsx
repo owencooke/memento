5731bea0109b5fcf64ec3be3fbb13db81e044990
Owen Cooke <ocooke@ualberta.ca>
2025-03-03 19:05:25 -0700

    fix delete button on photo

--- app/src/components/inputs/PhotoSelectGrid.tsx
+++ app/src/components/inputs/PhotoSelectGrid.tsx
@@ -31,7 +31,12 @@ export default function PhotoSelectGrid({ onChange }: PhotoSelectGridProps) {
         key: `photo-${index}`,
         photo,
       })),
-      { key: "add-button", photo: null },
+      {
+        key: "add-button",
+        photo: null,
+        disabledDrag: true,
+        disabledReSorted: true,
+      },
     ],
     [photos],
   );
@@ -62,19 +67,19 @@ export default function PhotoSelectGrid({ onChange }: PhotoSelectGridProps) {
         data={gridData}
         onDragRelease={handleReorderPhotos}
         renderItem={(item: GridItem) => (
-          <View className="p-1 flex-1 aspect-square">
+          <View className="px-1 py-2 flex-1 aspect-square">
             {item.photo ? (
               // Render a photo
               <View className="relative overflow-hidden rounded-md">
                 <Image
                   source={{ uri: item.photo.uri }}
-                  className="w-full h-full"
+                  className="w-auto h-full mt-2 mr-2"
                   alt=""
                   resizeMode="cover"
                 />
                 <Button
                   onPress={() => item.photo && removePhoto(item.photo)}
-                  className="absolute p-1 rounded-full top-1 right-1"
+                  className="absolute p-2 rounded-full top-0 right-0"
                   size="sm"
                 >
                   <ButtonIcon className="m-0 p-0" as={CloseIcon} />
@@ -84,7 +89,7 @@ export default function PhotoSelectGrid({ onChange }: PhotoSelectGridProps) {
               // Render the add button
               <Button
                 size="lg"
-                className="w-full h-full"
+                className="mt-2 mr-2 h-full"
                 action="secondary"
                 onPress={() => setShowActionsheet(true)}
               >
