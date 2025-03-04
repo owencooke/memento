ebf63bc7526c597f3936a464ebacccbf7d0259a4
Owen Cooke <ocooke@ualberta.ca>
2025-03-04 15:28:02 -0700

    abstract memento form into component

--- app/src/hooks/usePhotos.ts
+++ app/src/hooks/usePhotos.ts
@@ -10,9 +10,13 @@ import * as ImagePicker from "expo-image-picker";
 export type DeviceSource = "picker" | "camera";
 export type Photo = ImagePicker.ImagePickerAsset;
 
-export default function usePhotos() {
+interface UsePhotosProps {
+  initialPhotos?: Photo[];
+}
+
+export default function usePhotos({ initialPhotos = [] }: UsePhotosProps) {
   const [hasPermission, setHasPermission] = useState(false);
-  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [photos, setPhotos] = useState<Photo[]>(initialPhotos);
 
   useEffect(() => {
     (async () => {
