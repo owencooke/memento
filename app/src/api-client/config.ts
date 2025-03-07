80435217bcb565a43c8420b1dc5c79754a430708
Owen Cooke <90405643+owencooke@users.noreply.github.com>
2025-03-07 15:43:13 -0700

    App/Backend Tunneling for UWS (#296)
    * backend tunnel workaround for UWS wifi
    
    * add docs

--- app/src/api-client/config.ts
+++ app/src/api-client/config.ts
@@ -1,6 +1,12 @@
 import type { CreateClientConfig } from "./generated/client.gen";
 
 const getBaseUrl = () => {
+  const tunnelHost = process.env.EXPO_PUBLIC_TUNNEL_API_HOST;
+  // Check if backend API is being tunneled
+  if (tunnelHost) {
+    return `https://${tunnelHost}`;
+  }
+  // Use machine's host IP as host for backend
   const hostname = process.env.EXPO_PUBLIC_API_HOST ?? "127.0.0.1";
   return `http://${hostname}:8000/`;
 };
