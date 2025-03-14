d7c040e7ed51fe6abc101a6886bd8286e682aeb6
Owen Cooke <ocooke@ualberta.ca>
2025-03-13 22:34:41 -0600

    add stacked card

--- app/src/components/cards/CollectionCard.tsx
+++ app/src/components/cards/CollectionCard.tsx
@@ -16,9 +16,6 @@ export default function CollectionCard({
   mementos,
   variant = "default",
 }: CollectionCardProps) {
-  // Get thumbnail from first memento
-  const thumbnail = null;
-
   // Extract city from whole location
   const city = useMemo(
     () => (location ? location.split(",")[0].trim() : location),
@@ -28,34 +25,40 @@ export default function CollectionCard({
   const isMarker = variant === "marker";
 
   return (
-    <View
-      className={`flex flex-col rounded-xl shadow-hard-3 bg-background-0 ${
-        isMarker ? "w-[80px] p-[6px]" : "flex-1 gap-4 p-3"
-      }`}
-    >
-      <View className="aspect-square flex-1">
-        <Image
-          source={{ uri: "https://placehold.co/400.png" }}
-          className="h-full w-full"
-          alt=""
-          resizeMode="cover"
-        />
-      </View>
-      <View>
-        <Text size={isMarker ? "sm" : "lg"} className="font-bold text-center">
-          {title}
-        </Text>
-      </View>
-      {!isMarker && (
-        <View className="flex flex-row justify-between items-center mt-auto font-medium">
-          <Text className="text-left" size="sm">
-            {date}
+    <View className="p-4">
+      <View className="relative">
+        {/* Background Stacked Cards */}
+        <View className="absolute w-full h-full rounded-xl bg-background-0 shadow-hard-3  -rotate-3 top-2 -left-2" />
+        <View className="absolute w-full h-full rounded-xl bg-background-0 shadow-hard-3 rotate-3 -top-2 left-2" />
+        {/* Actual Card with Contents */}
+        <View
+          className={`relative flex flex-col rounded-xl shadow-hard-3 bg-background-0 ${
+            isMarker ? "w-[80px] p-[6px]" : "flex-1 gap-4 p-3"
+          }`}
+        >
+          <View className="aspect-square flex-1">
+            <Image
+              source={{ uri: "https://placehold.co/400.png" }}
+              className="h-full w-full"
+              alt=""
+              resizeMode="cover"
+            />
+          </View>
+          <Text size={isMarker ? "sm" : "lg"} className="font-bold text-center">
+            {title}
           </Text>
-          <Text className="text-right" size="sm">
-            {city}
-          </Text>
+          {!isMarker && (
+            <View className="flex flex-col justify-between items-center">
+              <Text className="text-left" size="sm">
+                {date}
+              </Text>
+              <Text className="text-right" size="sm">
+                {city}
+              </Text>
+            </View>
+          )}
         </View>
-      )}
+      </View>
     </View>
   );
 }
