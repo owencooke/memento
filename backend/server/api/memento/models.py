896f9284eb7992f762908488c2494421dd202f7e
Levi Thomas <levi.thomas5384@gmail.com>
2025-03-15 15:44:49 -0600

    pydantic model for memento filter query

--- backend/server/api/memento/models.py
+++ backend/server/api/memento/models.py
@@ -1,3 +1,8 @@
+import datetime
+from typing import Any
+
+from pydantic import BaseModel, Field, field_validator
+
 from server.services.db.models.gis import BaseWithCoordinates, CoordinatesInsert
 from server.services.db.models.schema_public_latest import (
     ImageInsert,
@@ -27,3 +32,22 @@ class UpdateMemento(CoordinatesInsert, BaseWithCoordinates, MementoUpdate):  # t
 
     Overrides coordinates with proper Pydantic model.
     """
+
+
+class MementoFilterParams(BaseModel):
+    """Filter parameters for Memento"""
+
+    start_date: datetime.date | None = Field(default=None)
+    end_date: datetime.date | None = Field(default=None)
+
+    @field_validator("end_date")
+    @classmethod
+    def check_date_order(
+        cls,
+        end_date: datetime.date,
+        values: dict[str, Any],
+    ) -> datetime.date:
+        """Checks that end date is gte start_date"""
+        if end_date and values.get("start_date") and end_date < values["start_date"]:
+            raise ValueError("end date must be greater than or equal to start date")
+        return end_date
