2ef22c0865c591e4165ac71c1b14786dcb802ec4
owencooke <ocooke@ualberta.ca>
2025-03-16 01:42:14 -0600

    attempt to fix auth bug through redirects

--- app/src/app/(app)/_layout.tsx
+++ app/src/app/(app)/_layout.tsx
@@ -8,7 +8,7 @@ import Header from "@/src/components/navigation/Header";
 export default function AppLayout() {
   const { session, isLoading } = useSession();
 
-  // You can keep the splash screen open, or render a loading screen like we do here.
+  // Checking auth session
   if (isLoading) {
     return (
       <Center className="w-full h-full">
@@ -17,15 +17,11 @@ export default function AppLayout() {
     );
   }
 
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
+  // Redirect back to sign in if no valid session
   if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
     return <Redirect href="/" />;
   }
 
-  // This layout can be deferred because it's not the root layout.
   return (
     <Stack
       screenOptions={{
