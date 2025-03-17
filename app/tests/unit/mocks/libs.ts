218c2b5447bbbec3644171bc03e2c278caf1e370
owencooke <ocooke@ualberta.ca>
2025-03-17 14:54:08 -0600

    add a supabase mock / fix redirect URL

--- app/tests/unit/mocks/libs.ts
+++ app/tests/unit/mocks/libs.ts
@@ -6,3 +6,14 @@ jest.mock("expo-auth-session", () => ({
   ...jest.requireActual("expo-auth-session"),
   makeRedirectUri: jest.fn(() => "http://localhost:8081/auth/redirect"),
 }));
+
+jest.mock("@/src/libs/supabase", () => {
+  return {
+    supabase: {
+      auth: {
+        signOut: jest.fn(),
+        onAuthStateChange: jest.fn(),
+      },
+    },
+  };
+});
