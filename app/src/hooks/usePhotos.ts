2bf8a481b77a2e8984774b23a13153d34f1b9c4e
owencooke <ocooke@ualberta.ca>
2025-03-21 16:03:04 -0600

    fix messed up photos merge

--- app/src/hooks/usePhotos.ts
+++ app/src/hooks/usePhotos.ts
@@ -149,10 +149,15 @@ const getPhotosFromDevice = async (source: DeviceSource): Promise<Photo[]> => {
   return await Promise.all(
     result.assets.map(async (photo) => {
       const { uri, mimeType } = await compressImage(photo);
+      const uniqueFilename = uniqueId("photo_");
       return {
         ...photo,
         uri,
         mimeType,
+        // Image uploads to server require a filename to pass validation
+        fileName: photo.fileName ?? `${uniqueFilename}.jpg`,
+        // Captured images may not have id (required for internal state management)
+        assetId: photo.assetId ?? uniqueFilename,
       };
     }),
   );
