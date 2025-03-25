046b44b011e2e3d4beefe3236eeda62493193f11
owencooke <ocooke@ualberta.ca>
2025-03-25 13:12:07 -0600

    fix bulk create not having camera provider

--- app/src/app/(app)/(screens)/memento/create/bulk.tsx
+++ app/src/app/(app)/(screens)/memento/create/bulk.tsx
@@ -14,7 +14,7 @@ import { Text } from "@/src/components/ui/text";
 import GroupedPhotoGrid, {
   PhotoWithGroup,
 } from "@/src/components/inputs/GroupedPhotoGrid";
-import { usePhotos } from "@/src/context/PhotoContext";
+import { CameraProvider, usePhotos } from "@/src/context/PhotoContext";
 import MementoForm, {
   defaultMementoFormValues,
 } from "@/src/components/forms/MementoForm";
@@ -255,21 +255,23 @@ export default function BulkCreateMemento() {
                   className="stroke-background-500"
                 />
               </Pressable>
-              <MementoForm
-                initialValues={{
-                  memento: {
-                    caption: editingGroup.caption,
-                    date: editingGroup.date,
-                    location: editingGroup.location,
-                  },
-                  photos: editingGroup.photos,
-                }}
-                submitButtonText="Save Changes"
-                isSubmitting={false}
-                photosEditable={false}
-                onSubmit={handleSaveGroupDetails}
-                FormHeader={`Memento #${editingGroup.groupId + 1}`}
-              />
+              <CameraProvider>
+                <MementoForm
+                  initialValues={{
+                    memento: {
+                      caption: editingGroup.caption,
+                      date: editingGroup.date,
+                      location: editingGroup.location,
+                    },
+                    photos: editingGroup.photos,
+                  }}
+                  submitButtonText="Save Changes"
+                  isSubmitting={false}
+                  photosEditable={false}
+                  onSubmit={handleSaveGroupDetails}
+                  FormHeader={`Memento #${editingGroup.groupId + 1}`}
+                />
+              </CameraProvider>
             </View>
           </ActionsheetContent>
         </Actionsheet>
