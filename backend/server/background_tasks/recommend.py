3b0c71fd9fc24212b662a05c64f1bdbef69b7465
Levi Thomas <levi.thomas5384@gmail.com>
2025-04-03 20:54:11 -0600

    Checking for rejected recommendations

--- backend/server/background_tasks/recommend.py
+++ backend/server/background_tasks/recommend.py
@@ -1,24 +1,42 @@
-import asyncio
+from random import choice
 
+from loguru import logger
 from pydantic import UUID4
 
 from server.api.websocket.manager import websocket_manager
 from server.api.websocket.models import WebSocketMessage, WSMessageType
 from server.services.cluster_memento.hdbscan import cluster_mementos
-from server.services.db.queries.memento import get_mementos_for_clustering
+from server.services.db.queries.clustering import (
+    get_mementos_for_clustering,
+    is_collection_rejected,
+)
 
 
 async def recommend_collection(user_id: UUID4) -> None:
     """Background task for recommeding a new collection via clustering."""
-    # TODO: replace with actual algorithm call
-    await asyncio.sleep(5)
     mementos = get_mementos_for_clustering(user_id)
-    rejected_recommendations = get_rejected_recommendations(user_id)
     clustered_memento_ids = cluster_mementos(mementos, min_cluster_size=3)
 
-    # TODO: check DB to see if recommendation was already given
+    logger.debug(f"Clustered memento ids: {clustered_memento_ids}")
 
+    # Check DB for previously rejected collections
+    valid_recommendations = [
+        memento_ids
+        for memento_ids in clustered_memento_ids.values()
+        if not is_collection_rejected(user_id, memento_ids)
+    ]
+    logger.debug(f"Valid recommendation: {valid_recommendations}")
+
+    # No valid recommendations
+    if not valid_recommendations:
+        return
+
+    # Choose a random valid recommendation
+    selected_recommendation = choice(valid_recommendations)
     await websocket_manager.send_message(
         user_id,
-        WebSocketMessage(type=WSMessageType.RECOMMENDATION, body=clustered_memento_ids),
+        WebSocketMessage(
+            type=WSMessageType.RECOMMENDATION,
+            body=selected_recommendation,
+        ),
     )
