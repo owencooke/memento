77368e370ec95b7dfb22c046395ff6d0197d3bb5
Levi Thomas <levi.thomas5384@gmail.com>
2025-04-06 11:39:23 -0600

    GIS Tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,6 +2,7 @@
 # Import fixtures to expose them to all other test files during runtime
 from tests.fixtures.collection import *
 from tests.fixtures.file import *
+from tests.fixtures.gis import *
 from tests.fixtures.hdbscan import *
 from tests.fixtures.image import *
 from tests.fixtures.memento import *
